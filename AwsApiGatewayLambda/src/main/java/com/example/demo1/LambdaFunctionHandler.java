ab8198f0bde15386ef5e877c8b11db334dc125fd
d962617 <tanmoy.banerjee@team.telstra.com>
2022-02-11 21:16:27 +0530

    initial commit

---
+++ AwsApiGatewayLambda/src/main/java/com/example/demo1/LambdaFunctionHandler.java
@@ -0,0 +1,40 @@
+package com.example.demo1;
+
+import java.util.Map;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class LambdaFunctionHandler
+		implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+
+	@Override
+	public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent input, Context context) {
+		Map<String, String> inputParams = input.getQueryStringParameters();
+
+		context.getLogger().log("Input request: " + input.toString());
+		context.getLogger().log("Input: " + inputParams);
+		String name = inputParams.get("name");
+		context.getLogger().log("Name is - " + name);
+		MyDto dto = new MyDto();
+		dto.setId("id1");
+		dto.setName("Tanmoyy");
+		dto.setAddress("Testing my address");
+
+		ObjectMapper objMapper = new ObjectMapper();
+		APIGatewayProxyResponseEvent responseEvent = new APIGatewayProxyResponseEvent();
+
+		try {
+			responseEvent.setBody(objMapper.writeValueAsString(dto));
+		} catch (JsonProcessingException e) {
+			e.printStackTrace();
+		}
+		responseEvent.setStatusCode(200);
+		return responseEvent;
+	}
+
+}
